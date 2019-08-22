ea9c94571b87f733936bfda69035dd09792d8722
Tiffany Ho <tiffany.ho@duke.edu>
2019-08-22 10:45:32 -0400

    modify download_file output structure

--- src/chip2probe.py
+++ src/chip2probe.py
@@ -1,6 +1,7 @@
 import urllib.request
 import os, subprocess, pathlib
 import configparser
+import datetime
 
 from tqdm import tqdm
 from collections import defaultdict
@@ -64,82 +65,60 @@ def download_url(url, output_path):
 
 def get_file_info(filename):
     file_info = defaultdict(dict)
-    antibody_info = {}
     with open(filename, "r") as f:
         next(f)
         for line in f.readlines():
             if line.rstrip() != "" and not line.startswith("#"):
-                items = line.strip().split("\t")
-                exp_num, chip_name, chip_id, file_access_num, quality  = items[0], items[1], items[2], items[3], items[4]
-                chip_name = chip_name + "_" + exp_num 
-                print(items) 
-                if len(items) > 5: # if it's a replicate, it would have output_type and antibody_num
-                    output_type = items[5]
-                    full_chip_id = create_full_id(chip_id, output_type)
-                    file_info[chip_name][full_chip_id] = (file_access_num, quality)
-                    antibody_info[chip_name] = items[6] # one antibody used for one experience
+                items = line.strip().split() # split on space and tab
+                exp_id, chip_name, rep_tag, file_id, quality, output_type, antibody_id  = \
+                items[0], items[1], items[2], items[3], items[4], items[5], items[6]
+                if rep_tag.startswith("r"):
+                    chip_name = chip_name + "_" + exp_id  
                 else:
-                    file_info[chip_name][chip_id] = (file_access_num, quality)
-    return file_info, antibody_info
-
-def create_full_id(chip_id, output_type):
-    full_chip_id = chip_id + "-"
-    if output_type == "alignments":
-        full_chip_id += "filtered"
-    elif output_type == "unfiltered_alignments":
-        full_chip_id += "unfiltered"
-    return full_chip_id
+                    corresponding_chip = items[7]
+                    chip_name = chip_name + "_"+ corresponding_chip
+                full_tag = rep_tag + "_" + output_type
+                file_info[chip_name][full_tag] = file_id
+    return file_info
 
 
-def download_chip(file_info, antibody_info, input_dir):
+def download_chip(file_info, input_dir):
 
-    config_chippath = configparser.ConfigParser()
+    filtered_file = configparser.ConfigParser()
+    unfiltered_file = configparser.ConfigParser()
     outdir = "../result/chipseq"
-    cntrl_chip_path = "%s/CONTROLS" % (outdir)
-    paths = [outdir, cntrl_chip_path]
-    for p in paths:
-        if not os.path.exists(p):
-            os.makedirs(p)
+    timestamp = ""
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-    for chipname, chip_download_ids in file_info.items():
+    for chipname, tag_fileID in file_info.items():
         chip_path = "%s/%s" % (outdir, chipname)
         if not os.path.exists(chip_path):
             os.makedirs(chip_path)
-        
-        saved_chip_path = {}
-        chip_info = chipname+"\n"
-        chip_info += "\tAntibody: %s\n" % antibody_info[chipname]
-        for chip_id, accessNum_quality in chip_download_ids.items():
-            isReplicate = False if chip_id.startswith("c") else True
-            fname = accessNum_quality[0] + ".bam"
-            quality = accessNum_quality[1]
-            if isReplicate:
-                saveto = "%s/%s" % (chip_path, fname)
-                print(chip_id)
-                output_type = chip_id.split("-")[1]
-            else:
-                saveto = "%s/%s" % (cntrl_chip_path, fname)
-            
-            if not os.path.exists(saveto):
-                chipurl = "https://www.encodeproject.org/files/{0}/@@download/{0}.bam".format(fname.split(".")[0])  
-                if isReplicate:
-                    chip_info += "\t%s: %s [quality = %s, output_type = %s] \n" % (chip_id, fname, quality, output_type)
-                    full_chip_id = create_full_id(chip_id, output_type)
-                    saved_chip_path[full_chip_id] = saveto
-                else: 
-                    chip_info += "\t%s: %s [quality = %s] \n" % (chip_id, fname, quality)
+        timestamp += chipname +"\n"
+        unfiltered, filtered = {}, {}
+        for tag, file_id in tag_fileID.items():
+            fname = file_id + ".bam"
+            saveto =  "%s/%s" % (chip_path, fname)
+            if not os.path.exists(saveto): 
+                if "unfiltered" in tag:
+                    unfiltered[tag.split("_")[0]] = fname
+                else:
+                    filtered[tag.split("_")[0]] = fname
+                chipurl = "https://www.encodeproject.org/files/{0}/@@download/{0}.bam".format(file_id)  
                 print("Downloading %s ..." % (fname))
                 download_url(chipurl, saveto)  # takes filename?!
-                  
-        with open("../result/chipseq/downloaded_chip_info.txt", 'a+') as f:
-            f.write(chip_info)
-            f.write("\n")
-        config_chippath[chipname] = saved_chip_path
-
-    with open(input_dir + 'chip_path.config', 'w') as configfile:
-        # output the chip_paths to input_dir for call_peaks() to use later
-        config_chippath.write(configfile)
-
+                now = datetime.datetime.now()
+                timestamp += "\tDownloaded %s on %s at %s \n" % (fname, now.strftime("%Y-%m-%d"), now.strftime("%H:%M:%S"))
+        filtered_file[chipname] = filtered
+        unfiltered_file[chipname] = unfiltered
+    with open("../result/chipseq/download_timestamp.txt", 'w') as f:
+        f.write(timestamp)
+    with open(input_dir + 'chips_unfiltered.config', 'w') as configfile:
+        # for call_peaks() to use later
+        unfiltered_file.write(configfile)
+    with open(input_dir + 'chips_filtered.config', 'w') as configfile:
+        filtered_file.write(configfile)
     
 
 def remove_inner_list(nested_list):
@@ -226,8 +205,9 @@ def main():
     if(config_pipeline['pipeline'].getboolean('downloadchip')):
         filename = config_pipeline['downloadchip_param']['chip_to_download']
         file = os.path.join(input_dir, filename)
-        file_info, antibody_info = get_file_info(file)
-        download_chip(file_info, antibody_info, input_dir)
+        file_info  = get_file_info(file)
+        download_chip(file_info, input_dir)
+        print("Finished downloading all files :)")
     else:
         print("Skipping downloadchip!")
 
