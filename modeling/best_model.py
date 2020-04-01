7ffdc5ef0f76ce847cdd021334dd7e197edd1876
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2020-04-01 02:54:24 -0400

    add pr curcve

--- modeling/best_model.py
+++ modeling/best_model.py
@@ -7,9 +7,13 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import roc_auc_score, accuracy_score
+import sklearn.metrics as metrics
 from sklearn.model_selection import StratifiedKFold
 import itertools
+import concurrent.futures as cc
+import timeit
+import os
+import functools
 
 class BestModel:
     def __init__(self, clf, param_dict, train_data, topn = -1, cv_fold_num=10, cv_num=1):
@@ -24,17 +28,17 @@ class BestModel:
         self.cv_fold_num = cv_fold_num
         self.cv_num = cv_num
 
-    def run_all(self):
+    def run_all(self, num_workers=os.cpu_count(), score_type="auc"):
         # initialize classifier
         self.init_data(self.train_data)
         # get best hyperparam combination
-        clf = self.get_best_param()
+        clf = self.get_best_param(num_workers=num_workers,score=score_type)
         new_x = self.train_data
         # get best top n
         if self.topn < len(self.train_data.columns) - 1:
-            new_x = self.set_topn()
+            new_x = self.set_topn(clf)
             # get best hyperparam for top n
-            clf = self.get_best_param()
+            clf = self.get_best_param(num_workers=num_workers,score=score_type)
         # return the best model for top n
         return new_x, clf
 
@@ -42,11 +46,11 @@ class BestModel:
         self.x_train = df.loc[:, df.columns != 'label']
         self.y_train = df.loc[:, 'label']
 
-    def set_clf(self, comb_dict):
+    def get_clf(self, comb_dict):
         if self.classifier == "RF":
-            self.clf = RandomForestClassifier(**comb_dict)
+            return RandomForestClassifier(**comb_dict)
         elif self.classifier == "DT":
-            self.clf = DecisionTreeClassifier(**comb_dict)
+            return DecisionTreeClassifier(**comb_dict)
         # d = {'RF': self.set_rf(comb),
         #      'DT': self.set_dt(comb)}
         # d[self.classifier]
@@ -64,66 +68,77 @@ class BestModel:
     # def set_nb(self, comb):
     #     pass
 
-    def get_best_param(self, save_to_file=False):
-        # find the best model for the data
+    def run_kfold(self, comb, score="auc"):
+        """
+        metrics: auc (Area Under ROC Curve)/pr (Precision Recall)
+        """
         x_train = self.x_train.values
         y_train = self.y_train.values
+        keys  = list(self.param_dict.keys())
 
+        comb_dict = {keys[i]:comb[i] for i in range(len(keys))}
+        clf = self.get_clf(comb_dict)
+        avg_acc = 0
+        avg_scr = 0
+        # take the average of runs
+        for i in range(self.cv_num):
+            # perform 10 fold cross validation
+            tot_acc = 0
+            tot_score = 0
+            cv = StratifiedKFold(n_splits=self.cv_fold_num, shuffle=True)
+            for train, test in cv.split(x_train, y_train):
+                # fit the model
+                model = clf.fit(x_train[train],y_train[train])
+                # get predictions
+                predict_label = model.predict(x_train[test])
+                predict_proba = model.predict_proba(x_train[test])[:,1]
+                # calculate metrics
+                tot_acc += metrics.accuracy_score(y_train[test], predict_label)
+                if score == "pr":
+                    tot_score += metrics.average_precision_score(y_train[test], predict_proba)
+                elif score == "auc" or score == "acc":
+                    # if accuracy we still return auc
+                    tot_score += metrics.roc_auc_score(y_train[test], predict_proba)
+                else:
+                    raise Exception("score metrics should be pr/auc/acc")
+            # calculate average metrics
+            acc = tot_acc/self.cv_fold_num
+            scr = tot_score/self.cv_fold_num
+            avg_acc += acc
+            avg_scr += scr
+        avg_acc = avg_acc/self.cv_num
+        avg_scr = avg_scr/self.cv_num
+        return {"params":comb_dict, "avg_acc": avg_acc, "avg_%s"%score: avg_scr}
+
+    def get_best_param(self, save_to_file=False, num_workers=os.cpu_count(), score="auc"):
+        print("==== Get best param, utilizing %d workers =====" % num_workers)
+        start_time = timeit.default_timer()
         comb_lst = list(self.param_dict.values())
         combinations = list(itertools.product(*comb_lst))
-        keys  = list(self.param_dict.keys())
-        output = []
 
         # loop through every hyperparameter combination
         max_auc = 0
-        for comb in tqdm(combinations):
-            comb_dict = {keys[i]:comb[i] for i in range(len(keys))}
-            self.set_clf(comb_dict)
-            avg_acc = 0
-            avg_auc = 0
-            # take the average of runs
-            for i in range(self.cv_num):
-                # perform 10 fold cross validation
-                tot_acc = 0
-                tot_auc = 0
-                cv = StratifiedKFold(n_splits=self.cv_fold_num, shuffle=True)
-                for train, test in cv.split(x_train, y_train):
-                    # fit the model
-                    model = self.clf.fit(x_train[train],y_train[train])
-                    # get predictions
-                    predict_label = model.predict(x_train[test])
-                    predict_proba = model.predict_proba(x_train[test])[:,1]
-                    # calculate metrics
-                    tot_acc += accuracy_score(y_train[test], predict_label)
-                    tot_auc += roc_auc_score(y_train[test], predict_proba)
-                # calculate average metrics
-                acc = tot_acc/self.cv_fold_num
-                auc = tot_auc/self.cv_fold_num
-                avg_acc += acc
-                avg_auc += auc
-            avg_acc = avg_acc/self.cv_num
-            avg_auc = avg_auc/self.cv_num
-            output.append([comb[0], comb[1], avg_acc, avg_auc])
-            if save_to_file:
-                # write preliminary output
-                columns = ["n_est", "max_depth", "acc", "auc"]
-                output_df = pd.DataFrame.from_records(output, columns = columns)
-                output_df.to_csv("rf_all_params_results_step1.csv", index=False)
+        run_kfold_partial = functools.partial(self.run_kfold, score=score)
+        with cc.ProcessPoolExecutor(max_workers = num_workers) as executor:
+            # TODO: update input to combinations to dictionary
+            output = list(tqdm(executor.map(run_kfold_partial, combinations), total=len(combinations)))
 
-            if avg_auc > max_auc:
-                best_comb = comb
-                max_auc = avg_auc
+            # if save_to_file:
+            #     # write preliminary output
+            #     columns = ["n_est", "max_depth", "acc", "auc"]
+            #     output_df = pd.DataFrame.from_records(output, columns = columns)
+            #     output_df.to_csv("rf_all_params_results_step1.csv", index=False)
 
-        print("Best params:", best_comb)
+        best = max(output, key=lambda x:x['avg_%s'%score])
 
-        self.set_clf(best_comb)
+        print("Best params (using %s): %s" % (score,str(best["params"])))
+        print("Total running time: %d seconds" % (timeit.default_timer() - start_time))
+        return self.get_clf(best["params"])
 
-        return self.clf
-
-    def set_topn(self):
-        model = self.clf.fit(self.x_train,self.y_train)
+    def set_topn(self, clf):
+        model = clf.fit(self.x_train,self.y_train)
         feat_impt = model.feature_importances_
-        print(sorted(zip(map(lambda x: round(x, 4), feat_impt), self.x_train.columns),
+        print("Top %d features"%self.topn,sorted(zip(map(lambda x: round(x, 4), feat_impt), self.x_train.columns),
              reverse=True)[:self.topn])
         # get the new x
         self.x_train = self.train_data.iloc[:, feat_impt.argsort()[::-1][:self.topn]]
--- modeling/model_select.py
+++ modeling/model_select.py
@@ -10,7 +10,6 @@ from sklearn import ensemble, model_selection, metrics, tree
 import pandas as pd
 import numpy as np
 import scipy
-from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import pickle
 
@@ -22,16 +21,18 @@ def get_numeric_label(training):
     return train
 
 
-def display_output(fpr_list, tpr_dict, auc_dict, path, title):
+def display_output(xy, auc_dict, path, title, score_type="auc"):
     """
         This plots the average ROC curve of all the classifiers in a single plot
     """
     plt.clf() # first, clear the canvas
-    plt.plot([0, 1], [0, 1], color="gray", alpha=0.5, lw=0.3)#linestyle="--",
-    for key in  tpr_dict:
-        tpr_list = tpr_dict[key]
+    if score_type == "pr":
+        print("here")
+        plt.plot([1, 0], [1, 0], color="gray", alpha=0.5, lw=0.3)#linestyle="--",
+    else:
+        plt.plot([0, 1], [0, 1], color="gray", alpha=0.5, lw=0.3)#linestyle="--",
+    for key in  xy:
         auc = auc_dict[key]
-
         #for key in fpr_dict:
         """
         ln = key.split(",")
@@ -42,11 +43,15 @@ def display_output(fpr_list, tpr_dict, auc_dict, path, title):
         else:
         plt.plot(fpr_list, tpr_list, linestyle="--", lw=1, label='%s: %.3f' % (key,auc))
         """
-        plt.plot(fpr_list, tpr_list, lw=2, label='%s: %.3f' % (key,auc))
+        plt.plot(xy[key]['x'], xy[key]['y'], lw=2, label='%s: %.3f' % (key,auc))
 
         # Show the ROC curves for all classifiers on the same plot
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
+        if score_type == "pr":
+            plt.xlabel('Recall')
+            plt.ylabel('Precision')
+        else:
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
 
     plt.grid()
     plt.title(title)
@@ -54,67 +59,89 @@ def display_output(fpr_list, tpr_dict, auc_dict, path, title):
     leg._legend_box.align = "left"
     plt.savefig(path)
 
-def plot_auc(x_train_dict, title="Average ROC Curves", plotname="auc.png"):
-    tpr_dict = {key:[] for key in x_train_dict}
-    auc_dict = {key:[] for key in x_train_dict}
-    acc_dict = {key:[] for key in x_train_dict}
+# plot_performance
+def plot_metrics(x_train_dict, title="Average ROC Curves",
+                 plotname="auc.png", n_splits=10, score_type="auc"):
+    yprob_dict = {key:[] for key in x_train_dict}
+    ytrue_dict = {key:[] for key in x_train_dict}
+    ypred_dict = {key:[] for key in x_train_dict}
+    # auc_dict = {key:[] for key in x_train_dict}
+    # acc_dict = {key:[] for key in x_train_dict}
 
-    fpr_lim=100
-    base_fpr = np.linspace(0, 1, 101)[:fpr_lim+1]
-    cv = model_selection.KFold(n_splits=10,shuffle=True)
+    x_lim=100
+    #base_x = np.linspace(0, 1, 101)
+    cv = model_selection.KFold(n_splits=n_splits,shuffle=True)
 
     random_x = next(iter(x_train_dict.values()))[0]
-    x_train = random_x.values.tolist() # .loc[:,random_x.columns != 'label']
-    y_train = random_x['label'].values
+    xt_all = random_x.values.tolist() # .loc[:,random_x.columns != 'label']
+    labels = random_x['label'].values
 
-    rf = ensemble.RandomForestClassifier(n_estimators=500, max_depth=10,random_state=0)
-    for train_idx,test_idx in cv.split(x_train):
-        tprs = []
+    for train_idx,test_idx in cv.split(xt_all):
         aucs_val = []
         # compare with just distance
         for key in x_train_dict:
             # need to convert this with index, somehow cannot do
             # x_train[train_idx] for multi features
             xt_df = x_train_dict[key][0]
-            xt = xt_df.loc[:,xt_df.columns != 'label'].values.tolist() # need to be a list of list
+            xt = np.array(xt_df.loc[:,xt_df.columns != 'label'].values.tolist()) # need to be a list of list
 
-            data_train = [xt[i] for i in train_idx]
-            data_test = [xt[i] for i in test_idx]
-            lbl_train = [y_train[i] for i in train_idx]
-            lbl_test = [y_train[i] for i in test_idx]
+            x_train, x_test = xt[train_idx], xt[test_idx]
+            y_train, y_test = labels[train_idx], labels[test_idx]
 
             model = x_train_dict[key][1]
-
-            model = model.fit(data_train, lbl_train)
-            y_score = model.predict_proba(data_test)
-            fpr, tpr, _ = metrics.roc_curve(lbl_test, y_score[:, 1])
+            model = model.fit(x_train, y_train)
+            y_score = model.predict_proba(x_test)
+            # if score_type == "auc":
+            #     x, y, _ = metrics.roc_curve(lbl_test, y_score[:, 1]) #fpr, tpr, threshold
+            # elif score_type == "pr":
+            #     y, x, _ = metrics.precision_recall_curve(lbl_test, y_score[:, 1]) # precision, recall, threshold
             #auc = metrics.roc_auc_score(lbl_test, y_score[:,1])
-            tpr = scipy.interp(base_fpr, fpr, tpr) # add points to the plotting
-            res_auc = metrics.auc(base_fpr, tpr)
-            tpr_dict[key].append(np.insert(tpr,0,0)) # insert 0 so we start the plot from the bottom left
-            auc_dict[key].append(res_auc)
+            #y = scipy.interp(base_x, x, y) # add points to the plotting
+            # res_auc = metrics.auc(x, y) #base_x
+            # auc_dict[key].append(res_auc)
 
-            y_pred = model.predict(data_test)
-            acc_dict[key].append(accuracy_score(lbl_test, y_pred))
+            ytrue_dict[key].append(y_test)
+            yprob_dict[key].append(y_score[:, 1])
+            ypred_dict[key].append(model.predict(x_test))
 
-    mean_tpr = {k:np.array(tpr_dict[k]).mean(axis=0) for k in tpr_dict}
-    mean_auc = {k:np.array(auc_dict[k]).mean(axis=0) for k in auc_dict}
-    mean_acc = {k:np.array(acc_dict[k]).mean(axis=0) for k in acc_dict}
-    print("Mean accuracy", mean_acc, "\nMean auc", mean_auc)
+            # y_pred = model.predict(data_test)
+            # acc_dict[key].append(accuracy_score(lbl_test, y_pred))
 
+    ytrue_dict = {k:np.concatenate(ytrue_dict[k]) for k in ytrue_dict}
+    yprob_dict = {k:np.concatenate(yprob_dict[k]) for k in yprob_dict}
+    ypred_dict = {k:np.concatenate(ypred_dict[k]) for k in ypred_dict}
+
+    if score_type == "auc":
+        mets = {k: metrics.roc_curve(ytrue_dict[k], yprob_dict[k]) for k in ytrue_dict} #fpr, tpr, threshold
+        for k in mets:
+            mets[k] = {"x":mets[k][0], "y":mets[k][1]} #"fpr, tpr"
+    elif score_type == "pr":
+        mets = {k: metrics.precision_recall_curve(ytrue_dict[k], yprob_dict[k]) for k in ytrue_dict} # precision, recall, threshold
+        for k in mets:
+            mets[k] = {"x":mets[k][1], "y":mets[k][0]} #recall, precision
+
+    auc = {k:metrics.auc(mets[k]['x'],mets[k]['y']) for k in ytrue_dict} #{k:np.array(auc_dict[k]).mean(axis=0) for k in auc_dict}
+    acc = {k:metrics.accuracy_score(ytrue_dict[k],ypred_dict[k]) for k in ytrue_dict}
+    print("Mean accuracy", acc, "\nMean auc (%s)" % score_type, auc)
     # left append 0 in base fpr just so we start at 0 (we did the same for the tpr)
-    base_fpr = np.insert(base_fpr,0,0)
-    display_output(base_fpr, mean_tpr, mean_auc, path=plotname, title=title)
+    # if score_type == "auc":
+    #     base_x = np.insert(base_x,0,0)
+    #     for k in y_dict:
+    #         mean_y[key] = np.insert(mean_y[key],0,0)
+    display_output(mets, auc, path=plotname, title=title, score_type=score_type)
 
 if __name__ == "__main__":
     trainingpath = "train1.tsv"
     #trainingpath = "trainingdata/training_new.csv"
     shapepath = "/Users/vincentiusmartin/Research/chip2gcPBM/probedata/191030_coop-PBM_Ets1_v1_2nd/dnashape/training_p01_adjusted_reversed"
     ds = DNAShape(shapepath)
+    score_type = "pr"
 
     rf_param_dict = {
                     'n_estimators': [500],
-    				'max_depth': [10]
+    				'max_depth': [5],
+                    "min_samples_leaf" : [10],
+                    "min_samples_split" : [10]
     			}
     dt_param_dict = {
     				"criterion" : ['gini', 'entropy'],
@@ -123,55 +150,53 @@ if __name__ == "__main__":
     			}
 
     df = pd.read_csv(trainingpath, sep="\t")
-    df = df[df["orientation"] == "TT"]
+    #df = df[df["orientation"] == "TT"]
 
     t = Training(df, corelen=4).flip_one_face_orientation(["GGAA","GGAT"])
 
 
     xtr = {
-            "dist":
-                BestModel(clf="RF",
-                          param_dict=rf_param_dict,
-                          train_data=t.get_training_df({
-                                  "distance":{"type":"numerical"},
-                                  "orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True, "include":"F"}
-                              })
-                ).run_all()
+            # "distance":
+            #     BestModel(clf="RF",
+            #               param_dict=rf_param_dict,
+            #               train_data=t.get_training_df({
+            #                       "distance":{"type":"numerical"}
+            #                       #"orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True}
+            #                   })
+            #     ).run_all(),
             # "flankshape":
             #     BestModel(clf="RF",
             #               param_dict=rf_param_dict,
             #               train_data=t.get_training_df({
             #                       "flankshape": {"ds":ds, "seqin":5, "smode":"positional"},
-            #                       "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"},
-            #                       "orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True, "include":"F"}
+            #                       "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"}
+            #                       #"orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True}
             #                   })
             #     ).run_all(),
-            # "dist-flankshape":
-            #     BestModel(clf="RF",
-            #               param_dict=rf_param_dict,
-            #               train_data=t.get_training_df({
-            #                       "distance":{"type":"numerical"},
-            #                        "flankshape": {"ds":ds, "seqin":5, "smode":"positional"},
-            #                        "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"},
-            #                        "orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True, "include":"F"}
-            #                   }),
-            #     ).run_all(),
-            #  "top10":
-            #  	BestModel(clf="RF",
-            #               param_dict=rf_param_dict,
-            #               train_data=t.get_training_df({
-            #                       "distance":{"type":"numerical"},
-            #                       "flankshape": {"ds":ds, "seqin":5, "smode":"positional"},
-            #                       "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"},
-            #                       "orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True, "include":"F"}
-            #                   }),
-            #                topn=10
-            #     ).run_all()
+            "dist-flankshape":
+                BestModel(clf="RF",
+                          param_dict=rf_param_dict,
+                          train_data=t.get_training_df({
+                                  "distance":{"type":"numerical"},
+                                   "flankshape": {"ds":ds, "seqin":5, "smode":"positional"},
+                                   "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"}
+                                   #"orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True}
+                              }),
+                ).run_all(score_type=score_type)
+             # "top10-dist-flankshape":
+             # 	BestModel(clf="RF",
+             #              param_dict=rf_param_dict,
+             #              train_data=t.get_training_df({
+             #                      "distance":{"type":"numerical"},
+             #                      "flankshape": {"ds":ds, "seqin":5, "smode":"positional"},
+             #                      "flankshape": {"ds":ds, "seqin":-3, "smode":"positional"}
+             #                      #"orientation": {"positive_cores":["GGAA","GGAT"], "one_hot":True}
+             #                  }),
+             #               topn=10
+             #    ).run_all()
         }
 
-
-
-    plot_auc(xtr, "Average ROC Curves Using RF for All Orientations", "dist_flank_seq_auc.png")
+    plot_metrics(xtr, "Average ROC Curves Using RF for All Orientations", "dist_flank_seq_auc.png",score_type=score_type)
     """
     # save the first model
     xt = t.get_feature_all({
