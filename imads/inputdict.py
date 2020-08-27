8ccb9eb617c062adbcae88459a457593547eda61
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2020-08-27 01:49:25 -0400

    input

--- imads/inputdict.py
+++ imads/inputdict.py
@@ -6,9 +6,9 @@ param = {
         "corelist" : ["GGAA", "GGAT"],
         "tfname" : "Ets1",
         "grid" : { #SVR feature
-            "c": [0.01],# 0.05, 0.1, 0.5, 1, 5, 10], # cost
-            "g": [0.0001],  #0.0005, 0.001, 0.005, 0.01, 0.1, 1, 10], # gamma for epsilon SVR
-            "p": [0.0001], # 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1], # epsilon, linear don't use: 0.01
+            "c": [0.01, 0.05, 0.1, 0.5, 1, 5, 10], # cost
+            "g": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1, 1, 10], # gamma for epsilon SVR
+            "p": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1], # epsilon, linear don't use: 0.01
             "t": [2] # kernel type: 0:linear, 1: polynomial, 2: radial, 3: sigmoid, 4: precomputed
         },
         "numfold" : 5,
